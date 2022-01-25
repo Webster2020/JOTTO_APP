d1e0b2ba6c6bf34e7275f8db253d3c3733a4215b
Michal Szwajgier <michalszwajgier@gmail.com>
2022-01-25 18:16:04 +0100

    Add App tests

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useEffect } from 'react';
 
 import Congrats from './components/Congrats/Congrats';
 import GuessedWords from './components/GuessedWords/GuessedWords';
 import Input from './components/Input/Input';
+
+import { getSecretWord } from './actions';
 
 function App() {
   // TODO: get props from shared state
   const success = false;
   const secretWord = 'party';
   const guessedWords = [];
+
+  useEffect(() => {
+    getSecretWord();
+  }, []);
 
   return (
     <div 
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { findByTestAttr } from './utils/testUtils';
 import { App } from './App';
 
+import { getSecretWord as mockGetSecretWord } from './actions';
+// activate global mock to make sure getSecretWord doesn't make network call
+jest.mock('./actions');
+
 const setup = () => {
-  return shallow(<App />);
+  // use mount because useEffect not called on 'shallow'
+  // https://github.com/airbnb/enzyme/issues/2086
+  return mount(<App />);
 };
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
   const wrapper = setup();
   const appComponent = findByTestAttr(wrapper, 'component-app');
   expect(appComponent).toHaveLength(1);
+});
+
+describe('get secret word', () => {
+  beforeEach(() => {
+    mockGetSecretWord.mockClear();
+  });
+  test('get secret word on app mount', () => {
+    const wrapper = setup();
+    console.log(wrapper);
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+  });
+  test('getSecretWord does not run on app update', () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+
+    // using setProps because wrapper.update() doesn't trigger useEffect
+    // https://github.com/enzymejs/enzyme/issues/2254
+    wrapper.setProps();
+
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+
+  });
 });
